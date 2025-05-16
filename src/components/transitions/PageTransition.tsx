3e498d2afc6c10865651a089ee7a65c1cac9f155
Jeremy Bobbin <jer@jer.cx>
2025-05-16 12:19:04 -0700

    remove page transition

--- src/components/transitions/PageTransition.tsx
+++ src/components/transitions/PageTransition.tsx
@@ -1,47 +1,10 @@
-
 import React from 'react';
-import { motion } from 'framer-motion';
 
-interface PageTransitionProps {
-  children: React.ReactNode;
-  className?: string;
-  style?: React.CSSProperties;
-}
-
-const pageVariants = {
-  initial: {
-    opacity: 0,
-    y: 10,
-  },
-  in: {
-    opacity: 1,
-    y: 0,
-  },
-  out: {
-    opacity: 0,
-    y: -10,
-  },
-};
-
-const pageTransition = {
-  type: 'tween',
-  ease: 'easeInOut',
-  duration: 0.3,
-};
-
-export const PageTransition = ({ children, className, style }: PageTransitionProps) => {
+export const PageTransition = ({children}) => {
   return (
-    <motion.div
-      initial="initial"
-      animate="in"
-      exit="out"
-      variants={pageVariants}
-      transition={pageTransition}
-      className={className || "w-full"}
-      style={style}
-    >
+    <React.Fragment>
       {children}
-    </motion.div>
+    </React.Fragment>
   );
 };
 
